e2eeebcdfcaa0301779bac09e7fa60acb18e0ae1
oypservices <41708206+oypservices@users.noreply.github.com>
2018-07-25 20:48:32 -0400

    Update IFI.js

--- IFI.js
+++ IFI.js
@@ -639,11 +639,3 @@ $(document).on('knack-scene-render.scene_264', function(event, scene) {
   console.log (JSON.stringify (scene));
 });
 
-
-
-// Change "scene_1" to the scene you want to listen for
-$(document).on('knack-scene-render.any', function(event, scene) {
-  // Do something after the scene renders
-  alert('listener for scene: ' + scene.key);
-  console.log (JSON.stringify (scene));
-});

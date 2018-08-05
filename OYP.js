e8a4f7912f0689c5a64a3d7d9ebeb19b0ecacf87
oypservices <41708206+oypservices@users.noreply.github.com>
2018-08-05 12:31:40 -0400

    Update OYP.js

--- OYP.js
+++ OYP.js
@@ -1,6 +1,6 @@
 
 function hideShowContactNoteFields(view, val) {
-	
+/*
 $('#kn-input-' + dbNotes.Date).show();
 $('#kn-input-' + dbNotes.Notes).show();
 $('#kn-input-' + dbNotes.Taskormeeting).show();
@@ -12,76 +12,34 @@ $('#kn-input-' + dbNotes.SalesRep).show();
 $('#kn-input-' + dbNotes.Site).show();
 $('#kn-input-' + dbNotes.Contact).show();
 $('#kn-input-' + dbNotes.Organization).show();
-
+*/
 
 	    // If this value in the form doesn't equal "SpecificValue" then prevent the form from submitting
     if (val == "Task") {
 	console.log (val);
 
-	$('#kn-input-' +  dbNotes.ContactNotedate).show();
-	$('#kn-input-' +  dbContactNotes.CaseManager).show();
-	$('#kn-input-' +  dbContactNotes.Client).show();
-	$('#kn-input-' +  dbContactNotes.NoteType).show();
-
-	$('#kn-input-' +  dbContactNotes.MeetingStatus).hide();
-	$('#kn-input-' +  dbContactNotes.ContactDateStart).show();
-	$('#kn-input-' +  dbContactNotes.ContactDateEnd).show();
-	$('#kn-input-' +  dbContactNotes.VisitLocation).hide();
-	$('#kn-input-' +  dbContactNotes.ReasonforContact).hide();
-	$('#kn-input-' +  dbContactNotes.ClientIRP).show();
-	$('#kn-input-' +  dbContactNotes.IRPGoals).show();
-	$('#kn-input-' +  dbContactNotes.ClientGoalInterventions).hide();
-	$('#kn-input-' +  dbContactNotes.PersonsPresent).hide();
-	$('#kn-input-' +  dbContactNotes.ClientResponses).show();
-	$('#kn-input-' +  dbContactNotes.PlanforNextVisit).show();
-	$('#kn-input-' +  dbContactNotes.OtherComment).hide();
-	$('#kn-input-' +  dbContactNotes.MedicationChanges).hide();
-	$('#kn-input-' +  dbContactNotes.CaseManagerSignature).show();
+	$('#kn-input-' + dbNotes.Taskormeeting).show();
+	$('#kn-input-' + dbNotes.TasksorMeetingTypes).show();
+	$('#kn-input-' + dbNotes.TaskMeetingDueDate).show();
+	$('#kn-input-' + dbNotes.TaskStatus).show();
+	$('#kn-input-' + dbNotes.TaskUpdate).show();
 	return true;
     }
     else if (val == "Meeting") {
 	console.log (val);
-	$('#kn-input-' +  dbContactNotes.ContactNotedate).show();
-	$('#kn-input-' +  dbContactNotes.CaseManager).show();
-	$('#kn-input-' +  dbContactNotes.Client).show();
-	$('#kn-input-' +  dbContactNotes.NoteType).show();
-
-	$('#kn-input-' +  dbContactNotes.MeetingStatus).hide();
-	$('#kn-input-' +  dbContactNotes.ContactDateStart).show();
-	$('#kn-input-' +  dbContactNotes.ContactDateEnd).show();
-	$('#kn-input-' +  dbContactNotes.VisitLocation).show();
-	$('#kn-input-' +  dbContactNotes.ReasonforContact).hide();
-	$('#kn-input-' +  dbContactNotes.ClientIRP).hide();
-	$('#kn-input-' +  dbContactNotes.IRPGoals).hide();
-	$('#kn-input-' +  dbContactNotes.ClientGoalInterventions).hide();
-	$('#kn-input-' +  dbContactNotes.PersonsPresent).hide();
-	$('#kn-input-' +  dbContactNotes.ClientResponses).hide();
-	$('#kn-input-' +  dbContactNotes.PlanforNextVisit).hide();
-	$('#kn-input-' +  dbContactNotes.OtherComment).hide();
-	$('#kn-input-' +  dbContactNotes.MedicationChanges).hide();
-	$('#kn-input-' +  dbContactNotes.CaseManagerSignature).hide();
+	$('#kn-input-' + dbNotes.Taskormeeting).show();
+	$('#kn-input-' + dbNotes.TasksorMeetingTypes).show();
+	$('#kn-input-' + dbNotes.TaskMeetingDueDate).show();
+	$('#kn-input-' + dbNotes.TaskStatus).show();
+	$('#kn-input-' + dbNotes.TaskUpdate).show();
 	return true;
     } 
     else {
-	$('#kn-input-' +  dbContactNotes.ContactNotedate).show();
-	$('#kn-input-' +  dbContactNotes.CaseManager).show();
-	$('#kn-input-' +  dbContactNotes.Client).show();
-	$('#kn-input-' +  dbContactNotes.NoteType).show();
-
-	$('#kn-input-' +  dbContactNotes.MeetingStatus).show();
-	$('#kn-input-' +  dbContactNotes.ContactDateStart).show();
-	$('#kn-input-' +  dbContactNotes.ContactDateEnd).show();
-	$('#kn-input-' +  dbContactNotes.VisitLocation).show();
-	$('#kn-input-' +  dbContactNotes.ReasonforContact).show();
-	$('#kn-input-' +  dbContactNotes.ClientIRP).show();
-	$('#kn-input-' +  dbContactNotes.IRPGoals).show();
-	$('#kn-input-' +  dbContactNotes.ClientGoalInterventions).show();
-	$('#kn-input-' +  dbContactNotes.PersonsPresent).show();
-	$('#kn-input-' +  dbContactNotes.ClientResponses).show();
-	$('#kn-input-' +  dbContactNotes.PlanforNextVisit).show();
-	$('#kn-input-' +  dbContactNotes.OtherComment).show();
-	$('#kn-input-' +  dbContactNotes.MedicationChanges).show();
-	$('#kn-input-' +  dbContactNotes.CaseManagerSignature).show();
+	$('#kn-input-' + dbNotes.Taskormeeting).hide();
+	$('#kn-input-' + dbNotes.TasksorMeetingTypes).hide();
+	$('#kn-input-' + dbNotes.TaskMeetingDueDate).hide();
+	$('#kn-input-' + dbNotes.TaskStatus).hide();
+	$('#kn-input-' + dbNotes.TaskUpdate).hide();
 	return true;
     }
 	  

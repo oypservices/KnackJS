863e46af5d4cc5ddccc74d7267090e95a856cb75
oypservices <41708206+oypservices@users.noreply.github.com>
2018-08-05 11:53:35 -0400

    Update OYPVariables.js

--- OYPVariables.js
+++ OYPVariables.js
@@ -1,1 +1,37 @@
 
+var api_url = 'https://api.knack.com/v1/scenes/';
+var api_urlpg = 'https://api.knack.com/v1/pages/';
+var app_id = Knack.app.id;
+var user = Knack.getUserToken();
+var headers = { "Authorization": user, "X-Knack-Application-ID": app_id, "Content-Type":"application/json"};
+
+
+//Views
+var vw_notes	= [ 'view_54', 'view_13','view_133' ] ;
+
+
+
+// Database Model
+
+var dbClients = {
+  "ClientName":"field_2",
+"CaseManager":"field_139",
+"ReferredByTitle":"field_192",
+"ReferredBy":"field_118",
+"ReferredBy_raw":"field_118_raw",
+"ReferrerPhone":"field_120",
+"GuardianName":"field_122"
+
+}
+
+
+
+//standard URLs
+
+var urlClientTeamAdd = api_url + sc_contact_scene + '/views/' + vw_client_team_add + '/records/';
+var urlContactAdd = api_url + sc_contact_scene + '/views/' + vw_contact_add + '/records';
+var urlClientTeamList = api_url + sc_contact_scene + '/views/' + vw_client_team_list + '/records';
+var urlAccountAdd = api_url + sc_contact_scene + '/views/' + vw_account_add + '/records';
+var urlInterventionUpdate = api_url + sc_contact_scene + '/views/' + vw_goal_intervention_goalupdate + '/records';
+var urlGoalUpdate = api_url + sc_contact_scene + '/views/' + vw_goal_update + '/records';
+var urlInterventionList = api_url + sc_contact_scene + '/views/' + vw_goal_intervention_list + '/records';

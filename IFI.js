01e5aa45dc967f7a3fd25df9a1da2ca9743da4e8
oypservices <41708206+oypservices@users.noreply.github.com>
2018-09-08 15:28:37 -0400

    replaced jsonp

--- IFI.js
+++ IFI.js
@@ -750,7 +750,7 @@ necessary in order to display the interventions in a view / print type scenario.
         headers: headers,
 				data: senddata ,
 				crossDomain: true,
-				datatype: jsonp,
+				datatype: json,
         success: function (response) {
 
 					console.log ( JSON.stringify(response)) ;

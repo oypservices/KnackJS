05ff5db26eb7a02c4bb4aa843048dea1d63518ba
oypservices <41708206+oypservices@users.noreply.github.com>
2018-09-09 11:15:35 -0400

    submit for view_209 - nnotes

--- OYP.js
+++ OYP.js
@@ -11,20 +11,20 @@ $('#kn-input-' + dbNotes.TaskUpdate).show();
 
 
 */
-	
 
-	
+
+
 try
 {
 
-	
+
 	$('#kn-input-' + dbNotes.SalesRep).hide();
 	$('#kn-input-' + dbNotes.Site).hide();
 	$('#kn-input-' + dbNotes.Contact).hide();
 	$('#kn-input-' + dbNotes.Organization).hide();
 	$('#kn-input-' + dbNotes.Project).hide();
 	$('#kn-input-' + dbNotes.ProjectItem).hide();
-	
+
 	// If this value in the form doesn't equal "SpecificValue" then prevent the form from submitting
     if (val == "Task") {
 	console.log (val);
@@ -44,7 +44,7 @@ try
 	$('#kn-input-' + dbNotes.TaskStatus).show();
 	$('#kn-input-' + dbNotes.TaskUpdate).show();
 	return true;
-    } 
+    }
     else {
 	$('#kn-input-' + dbNotes.Taskormeeting).hide();
 	$('#kn-input-' + dbNotes.TasksorMeetingTypes).hide();
@@ -53,18 +53,18 @@ try
 	$('#kn-input-' + dbNotes.TaskUpdate).hide();
 	return true;
     }
-	
+
 }catch (e)
     {
   console.error(e);
   console.error(e.stack) ;
     }
-	  
-	
+
+
 }
 
 $(document).on('knack-view-render.any' , function(event, view, data) {
-	       
+
   try {
 	var view_name =  view.key ;
 
@@ -73,17 +73,17 @@ $(document).on('knack-view-render.any' , function(event, view, data) {
 		if (view.source.object == "object_2" ) {
 			console.log (JSON.stringify (view)) ;
 			var fld_note_type =  view.key + '-' + dbNotes.NoteType;
-			
+
 			//add onchange event to the NoteType drop down box
 			$('#' + fld_note_type).on('change',function(e){
 			  console.log (e);
 			  console.log ($('#' + fld_note_type).val());
 			  hideShowContactNoteFields (  view, $('#'+ fld_note_type).val() );
 			});
-			
+
 			//call the onchange event the first time to set initial fields
 			hideShowContactNoteFields (  view, $('#'+ fld_note_type).val() );
-			
+
 		   	break;
 		}
 
@@ -93,5 +93,22 @@ $(document).on('knack-view-render.any' , function(event, view, data) {
 	  console.error(e);
 	  console.error(e.stack) ;
     }
-	    
+
+});
+
+$(document).on('knack-view-submit.view_209' , function(event, view, data) {
+
+  try {
+	var view_name =  view.key ;
+
+	console.log(data) ;
+	console.log(event);
+	console.log(view);
+
+  }catch (e)
+    {
+	  console.error(e);
+	  console.error(e.stack) ;
+    }
+
 });

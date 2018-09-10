ba2e24f9782f44b0fe6e1e080df1e1f28776a41f
oypservices <41708206+oypservices@users.noreply.github.com>
2018-09-10 01:18:01 -0400

    trying without the stringify

--- Helper.js
+++ Helper.js
@@ -1,47 +1,4 @@
 
-function  OYPServicesAPIPost( resource, headers, data )
-{
-		return new Promise ((resolve, reject) => {
-
-			var this_url = 'https://x247dlqfx2.execute-api.us-east-1.amazonaws.com/v1/' + resource ;
-		  console.log (this_url) ;
-			console.log (JSON.stringify(data)) ;
-			// Search to see if a contact exist by this name
-
-			$.ajax({
-						url: this_url ,
-						type: 'POST',
-						headers: headers,
-						data: JSON.stringify( data) ,
-						crossDomain: true,
-						datatype: 'json',
-						json: true,
-						success: function (response) {
-
-							if (resource == "jsontransform") {
-								var strMessage = JSON.stringify(response.body);
-								strMessage = strMessage.replace(/\\n/g, "");
-								strMessage = strMessage.replace(/\\/g, "");
-								console.log (strMessage) ;
-								console.log (strMessage.substring(1, strMessage.length - 1));
-								response = strMessage.substring(1, strMessage.length - 1) ;
-						  }
-
-
-							console.log ( JSON.stringify(response)) ;
-							resolve(response) ;
-
-						} ,
-				error: function (responseData, textStatus, errorThrown) {
-						alert('POST failed.');
-						reject();
-				}// end response function
-
-			}); //end ajax
-
-		}); // end promise
-
-} ;
 
 
 function parseUri (str) {
@@ -74,3 +31,49 @@ parseUri.options = {
 		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
 	}
 };
+
+
+
+function  OYPServicesAPIPost( resource, headers, data )
+{
+		return new Promise ((resolve, reject) => {
+
+			var this_url = 'https://x247dlqfx2.execute-api.us-east-1.amazonaws.com/v1/' + resource ;
+		  console.log (this_url) ;
+			console.log (data) ;
+			// Search to see if a contact exist by this name
+
+			$.ajax({
+						url: this_url ,
+						type: 'POST',
+						headers: headers,
+						data:  data ,
+						crossDomain: true,
+						datatype: 'json',
+						json: true,
+						success: function (response) {
+
+							if (resource == "jsontransform") {
+								var strMessage = JSON.stringify(response.body);
+								strMessage = strMessage.replace(/\\n/g, "");
+								strMessage = strMessage.replace(/\\/g, "");
+								console.log (strMessage) ;
+								console.log (strMessage.substring(1, strMessage.length - 1));
+								response = strMessage.substring(1, strMessage.length - 1) ;
+						  }
+
+
+							console.log ( JSON.stringify(response)) ;
+							resolve(response) ;
+
+						} ,
+				error: function (responseData, textStatus, errorThrown) {
+						alert('POST failed.');
+						reject();
+				}// end response function
+
+			}); //end ajax
+
+		}); // end promise
+
+} ;

6da851cd21f2ef3c4c6cf6ac2d64a2643057a477
oypservices <41708206+oypservices@users.noreply.github.com>
2018-09-10 01:22:34 -0400

    stuck now

--- Helper.js
+++ Helper.js
@@ -40,7 +40,7 @@ function  OYPServicesAPIPost( resource, headers, data )
 
 			var this_url = 'https://x247dlqfx2.execute-api.us-east-1.amazonaws.com/v1/' + resource ;
 		  console.log (this_url) ;
-			console.log (data) ;
+	//		console.log (data) ;
 			// Search to see if a contact exist by this name
 
 			$.ajax({
@@ -57,8 +57,8 @@ function  OYPServicesAPIPost( resource, headers, data )
 								var strMessage = JSON.stringify(response.body);
 								strMessage = strMessage.replace(/\\n/g, "");
 								strMessage = strMessage.replace(/\\/g, "");
-								console.log (strMessage) ;
-								console.log (strMessage.substring(1, strMessage.length - 1));
+					//			console.log (strMessage) ;
+					//			console.log (strMessage.substring(1, strMessage.length - 1));
 								response = strMessage.substring(1, strMessage.length - 1) ;
 						  }
 

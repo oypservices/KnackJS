f1ef5e1974100f514b1b79ca677c92664b2c1a80
oypservices <41708206+oypservices@users.noreply.github.com>
2018-09-16 16:58:01 -0400

    removed knack function

--- Helper.js
+++ Helper.js
@@ -36,10 +36,10 @@ parseUri.options = {
 
 function  OYPServicesAPIPost( resource, headers, data) {
 	var this_url = 'https://x247dlqfx2.execute-api.us-east-1.amazonaws.com/v1/'  ;
-	OYPServicesAPIPost( this_url, resource, headers, data ) ;
+	OYPServicesAPIPostURL( this_url, resource, headers, data ) ;
 }
 
-function  OYPServicesAPIPost( this_url, resource, headers, data )
+function  OYPServicesAPIPostURL( this_url, resource, headers, data )
 {
 		return new Promise ((resolve, reject) => {
 
@@ -78,44 +78,6 @@ function  OYPServicesAPIPost( this_url, resource, headers, data )
 				error: function (responseData, textStatus, errorThrown) {
 						alert('POST failed.');
 						reject(errorThrown);
-				}// end response function
-
-			}); //end ajax
-
-		}); // end promise
-
-} ;
-
-
-function  OYPServicesAPIKnack( url, resource, headers, filters )
-{
-		return new Promise ((resolve, reject) => {
-
-			if (typeof filters == "string") {
-				filters = JSON.parse(filters);
-			}
-
-
-			var this_url = url + resource + '?filters=' + encodeURIComponent(JSON.stringify(filters));
-		  console.log (this_url) ;
-			console.log (JSON.stringify(data)) ;
-
-			$.ajax({
-						url: this_url ,
-						type: 'GET',
-						headers: headers,
-						crossDomain: true,
-//						datatype: 'json',
-//						json: true,
-						success: function (response) {
-
-							console.log ( JSON.stringify(response)) ;
-							resolve(response) ;
-
-						} ,
-				error: function (responseData, textStatus, errorThrown) {
-						alert('GET failed.');
-						reject();
 				}// end response function
 
 			}); //end ajax

e3ac99efe7f5d62b27d54b1e48932ebc9313a1c2
oypservices <41708206+oypservices@users.noreply.github.com>
2018-09-30 20:54:03 -0400

    add contactnotes to render any logic

--- IFI.js
+++ IFI.js
@@ -772,172 +772,6 @@ necessary in order to display the interventions in a view / print type scenario.
 }
 
 
-/* Send Test Mail */
-function sendTTestMail ( ) {
-/* This functions reads the list of intervention by goal id, and nest them within the intervention field on the goal table.  This is
-necessary in order to display the interventions in a view / print type scenario.
-*/
-
-
-    var resource = 'sendmail'
-  	var data = {
-
-			  "to": "brian@oypservices.com",
-			  "subject": "Its working....Email sent successfully",
-			  "templateId": "d-dbd4fd2a6cbf42c6837e8198ca9564b0",
-			  "html": "data",
-			  "dynamic_template_data": {
-			    "total": "$ 239.85",
-			    "items": [
-			      {
-			        "text": "Nike Sneakers",
-			        "image": "https://marketing-image-production.s3.amazonaws.com/uploads/8dda1131320a6d978b515cc04ed479df259a458d5d45d58b6b381cae0bf9588113e80ef912f69e8c4cc1ef1a0297e8eefdb7b270064cc046b79a44e21b811802.png",
-			        "price": "$ 79.95"
-			      },
-			      {
-			        "text": "Old Line Sneakers",
-			        "image": "https://marketing-image-production.s3.amazonaws.com/uploads/3629f54390ead663d4eb7c53702e492de63299d7c5f7239efdc693b09b9b28c82c924225dcd8dcb65732d5ca7b7b753c5f17e056405bbd4596e4e63a96ae5018.png",
-			        "price": "$ 79.95"
-			      },
-			      {
-			        "text": "Blue Line Sneakers",
-			        "image": "https://marketing-image-production.s3.amazonaws.com/uploads/00731ed18eff0ad5da890d876c456c3124a4e44cb48196533e9b95fb2b959b7194c2dc7637b788341d1ff4f88d1dc88e23f7e3704726d313c57f350911dd2bd0.png",
-			        "price": "$ 79.95"
-			      }
-			    ],
-			    "emailsubject": "New API Gateway Subject",
-			    "receipt": true,
-			    "name": "Sample Name",
-			    "address01": "1234 Fake St.",
-			    "address02": "Apt. 123",
-			    "city": "Place",
-			    "state": "CO",
-			    "zip": "80202"
-			  }
-    };
-
-
-		OYPServicesAPIPost( resource, headers, data ) ;
-
-    resource = 'jsontransform';
-		data = {
-  "data": {
-    "discharge_summary": {
-      "_uid": "16cad9dd-cc4b-42f8-b7b2-980835d9e977::ripple_osi.ehrscape.c4h::1",
-      "language|code": "en",
-      "language|terminology": "ISO_639-1",
-      "territory|code": "GB",
-      "territory|terminology": "ISO_3166-1",
-      "context": {
-        "_health_care_facility|id": "904",
-        "_health_care_facility|id_scheme": "iEHR",
-        "_health_care_facility|id_namespace": "iEHR",
-        "_health_care_facility|name": "St.James's Hospital (Dublin)",
-        "patient_identifiers": {
-          "mrn": "9999999000",
-          "mrn|issuer": "iEHR",
-          "mrn|assigner": "iEHR",
-          "mrn|type": "MRN",
-          "oth": "1020714",
-          "oth|issuer": "iEHR",
-          "oth|assigner": "iEHR",
-          "oth|type": "OTH",
-          "gms": "-",
-          "gms|issuer": "iEHR",
-          "gms|assigner": "iEHR",
-          "gms|type": "GMS"
-        },
-        "start_time": "2010-05-14T00:00:00Z",
-        "setting|code": "238",
-        "setting|value": "other care",
-        "setting|terminology": "openehr"
-      },
-      "discharge_details": {
-        "discharge_details_uk_v1": {
-          "responsible_professional": {
-            "professional_name": {
-              "name": "COOKE MR FIACHRA"
-            },
-            "professional_identifier": "4547",
-            "professional_identifier|issuer": "iEHR",
-            "professional_identifier|assigner": "iEHR",
-            "professional_identifier|type": "MCN"
-          },
-          "language|code": "en",
-          "language|terminology": "ISO_639-1",
-          "encoding|code": "UTF-8",
-          "encoding|terminology": "IANA_character-sets"
-        }
-      },
-      "diagnoses": {
-        "problem_diagnosis": [
-          {
-            "problem_diagnosis_name": "Cholecystectomy",
-            "problem_diagnosis_status": {
-              "diagnostic_status|code": "at0017",
-              "diagnostic_status|value": "Working",
-              "diagnostic_status|terminology": "local"
-            },
-            "language|code": "en",
-            "language|terminology": "ISO_639-1",
-            "encoding|code": "UTF-8",
-            "encoding|terminology": "IANA_character-sets"
-          }
-        ]
-      },
-      "clinical_summary": {
-        "clinical_synopsis": {
-          "synopsis": "ADMISSION REASON: Admit with acute abdominal pain, deranged LFTs, normal amylase DIAGNOSIS: Cholecystectomy PROBLEMS: Abdominal pain PROBLEMS: Gallstones THEATRE PROCS: Lap Chole NON THEATRE PROCS: None LAB INVESTIGATIONS: As attached - FBC, UE, LFTs, Amylase RAD INVESTIGATIONS: As attached - USS Abdomen, MRCP OTHER INVESTIGATIONS: None PROGRESS DURING STAY: Uncomplicated post operative recovery.Full diet tolerated, wound sites dry and intact, no oozing. Vital signs normal, apyrexial. Mobilising/teds/clexane. No c/o abdominal pain. C/O right shoulder tip pain - advised post operative complication and should resolve within several days. Normal MRCP pre-op. Dx = acute cholecystitis with transiemt choledocholithiasis. ALLERGIES: NKDA DISCHARGE MEDICATION: MEDICATION:Refused analgesia on d/c INFO GIVEN TO PATIENT: All results and surgery as above explained. For removal of clips in 10/7 in dressing clinic - appt given. Avoid constipation OPD FOLLOW UP: 6/52 GP ACTIONS: Routine follow up",
-          "language|code": "en",
-          "language|terminology": "ISO_639-1",
-          "encoding|code": "UTF-8",
-          "encoding|terminology": "IANA_character-sets"
-        }
-      },
-      "composer|id": "023781",
-      "composer|id_scheme": "Medical Council No",
-      "composer|id_namespace": "iEHR",
-      "composer|name": "McCrea, Siobhan"
-    }
-  },
-  "template": {
-    "sourceId": "{{discharge_summary._uid}}",
-    "author_name": "{{discharge_summary['composer|name']}}",
-    "author_id": "{{discharge_summary['composer|id']}}",
-    "author_idScheme": "{{discharge_summary['composer|id_scheme']}}",
-    "documentDate": "{{discharge_summary.context.start_time}}",
-    "facility": "{{discharge_summary.context['_health_care_facility|name']}}",
-    "patientIdentifier_mrn": "{{discharge_summary.context.patient_identifiers.mrn}}",
-    "patientIdentifier_mrnType": "{{discharge_summary.context.patient_identifiers['mrn|type']}}",
-    "patientIdentifier_oth": "{{discharge_summary.context.patient_identifiers.oth}}",
-    "patientIdentifier_othType": "{{discharge_summary.context.patient_identifiers['oth|type']}}",
-    "patientIdentifier_gms": "{{discharge_summary.context.patient_identifiers.gms}}",
-    "patientIdentifier_gmsType": "{{discharge_summary.context.patient_identifiers['gms|type']}}",
-    "responsibleProfessional_name": "{{discharge_summary.discharge_details.discharge_details_uk_v1.responsible_professional.professional_name.name}}",
-    "responsibleProfessional_id": "{{discharge_summary.discharge_details.discharge_details_uk_v1.responsible_professional.professional_identifier}}",
-    "responsibleProfessional_idType": "{{discharge_summary.discharge_details.discharge_details_uk_v1.responsible_professional['professional_identifier|type']}}",
-    "dischargingOrganisation": "{{discharge_summary.discharge_details.discharge_details_uk_v1.discharging_organisation.name_of_organisation}}",
-    "dateTimeOfDischarge": "{{discharge_summary.discharge_details.discharge_details_uk_v1.discharging_organisation.name_of_organisatio.date_time_of_discharge}}",
-    "clinicalSynopsis": "{{discharge_summary.clinical_summary.clinical_synopsis.synopsis}}",
-    "dateOfAdmission": "{{discharge_summary.admission_details.inpatient_admission.date_of_admission}}",
-    "diagnosisList": [
-      "{{discharge_summary.diagnoses.problem_diagnosis}}",
-      {
-        "problem": "{{problem_diagnosis_name}}",
-        "description": "{{problem_diagnosis_status['diagnostic_status|value']}}",
-        "terminology": "{{problem_diagnosis_status['diagnostic_status|terminology']}}",
-        "terminologyCode": "{{problem_diagnosis_status['diagnostic_status|code']}}"
-      }
-    ]
-  }
-};
-	var body = OYPServicesAPIPost( resource, headers, data ) ;
-	console.log (body.body.sourceId) ;
-
-} ;
-
-
-
 /* Intervention Viees - Adds and Updates */
 
 $(document).on('knack-form-submit.view_268' , function(event, view, data) {
@@ -982,6 +816,19 @@ $(document).on('knack-view-render.any' , function(event, view, data) {
          setClientStatusText() ;
          return ;
        }
+
+       //ContactNotes
+       if (view.source.object == "object_4" ){
+
+          var fld_note_type =  view.key + '-field_236';
+         	$('#' + fld_note_type).on('change',function(e){
+         	  console.log (e);
+         	  console.log ($('#' + fld_note_type).val());
+         	  hideShowContactNoteFields (  view, $('#'+ fld_note_type).val() );
+         	});
+          return ;
+        }
+
      }
 
   catch (e)  {

9632ffe595ca92f1ec5be7d0acadd9be6ed96c92
oypservices <41708206+oypservices@users.noreply.github.com>
2018-10-01 22:31:45 -0400

    save as 300

--- IFI.js
+++ IFI.js
@@ -816,7 +816,7 @@ $(document).on('knack-view-render.any' , function(event, view, data) {
          setClientStatusText() ;
 
          //FORMAT ID  FIELD
-         $('#field_243').css("width", "50");
+         $('#field_243').css("width", "300");
          $('#field_243').keyup(function() {
              var val = this.value.replace(/\D/g, '');
              var newVal = '';

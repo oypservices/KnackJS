505904ede143c9ce84a0d9366c1d7d96b474ec82
oypservices <41708206+oypservices@users.noreply.github.com>
2018-12-28 16:55:17 -0500

    missed comma

--- OYPdbObjects.js
+++ OYPdbObjects.js
@@ -320,8 +320,8 @@ var Activities = {
   "name" : "activities",
   "conditionalDisplayFields" :[{
           "key" :"Activity Type" , "fieldlist" :  [
-          {"key" : "Task" , "fields" : ["Taskormeeting", "TasksorMeetingTypes", "TaskMeetingDueDate", "TaskStatus"."TaskUpdate"] },
-          { "key" : "Meeting" , "fields" :  ["Taskormeeting", "TasksorMeetingTypes", "TaskMeetingDueDate", "TaskStatus"."TaskUpdate"] }
+          {"key" : "Task" , "fields" : ["Taskormeeting", "TasksorMeetingTypes", "TaskMeetingDueDate", "TaskStatus","TaskUpdate"] },
+          { "key" : "Meeting" , "fields" :  ["Taskormeeting", "TasksorMeetingTypes", "TaskMeetingDueDate", "TaskStatus","TaskUpdate"] }
         ] },
         { "key": "Roles"  , "fieldlist" : [
           { "key" :"System" , "fields" : ["User", "Site", "Contact Name Expression"] }

d61cfe203231a78f38e90a4894a3b63607b28855
oypservices <41708206+oypservices@users.noreply.github.com>
2019-01-05 17:45:43 -0500

    goal clean up

--- IFIClientGoals.js
+++ IFIClientGoals.js
@@ -96,6 +96,7 @@ function copyGoalRecords (IRPId, resultNewIRP) {
 
 								delete (record.id) ;
 								record[dbGoals.ClientIRP] = IRPId;
+								delete (record[Interventions]) ;
 
 								var postapidata = {
 					 						"method": "post",
